b2b089da112506f2c0bd45b8284275fb6c2045a3
DulceMani <dulcenataly26@gmail.com>
2023-07-18 11:36:44 -0600

    Backend funcionando
    Se dividieron en dos el docker file

--- frontend/ManiTiendita/vite.config.js
+++ frontend/ManiTiendita/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    
+     host: true,
+     strictPort: true,
+     port: 3001, // This is the port which we will use in docker,
+     //hmr: { host: 'localhost', },
+    watch: {
+      usePolling: true,
+    },
+  }
 })
